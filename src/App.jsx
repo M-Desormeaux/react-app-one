fdb8739d38faa443b230833c185501df0634bee1
Michael <michael.desormeaux@scswla.org>
2021-06-24 08:31:59 -0500

    feat:

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { ExpenseItem } from './components/ExpenseItem';
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+    :root {
+        font-size: 16px;
+    }
+    html, body {
+        width: 100vw;
+        height: 100vh;
+        padding: 0;
+        margin: 0;
+    }
+`;
 
 export const App = () => {
 
     return (
         <>
+            <GlobalStyle />
             <h1>Insert your code 💕</h1>
+            <ExpenseItem />
+            <ExpenseItem />
+            <ExpenseItem />
+            <ExpenseItem />
         </>
     );
 };
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ExpenseItem = () => {
+
+    return (
+        <>
+            <h2>uwu</h2>
+        </>
+    );
+};
