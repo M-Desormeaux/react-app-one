1952c5c0a7bb9a8f19a02bcf9f2c8caf22ada913
Michael <michael.desormeaux@scswla.org>
2021-06-28 09:04:56 -0500

    refactor: 🎨

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AppContainer, AppWrapper, GlobalStyle } from "./AppStyle";
-import { ExpenseItem } from "./components/ExpenseItem/ExpenseItem";
+import { ExpenseList } from "./components/ExpenseList/ExpenseList";
 import { PageTitle } from "./components/PageTitle/PageTitle";
 
 export const App = () => {
@@ -9,36 +9,7 @@ export const App = () => {
             <AppContainer>
                 <GlobalStyle />
                 <PageTitle title="Expense Tracker" />
-                <ExpenseItem
-                    title="Toilet Paper"
-                    amount="94.12"
-                    date="2020, 7, 14"
-                />
-                <ExpenseItem
-                    title="New TV"
-                    amount="799.49"
-                    date="2021, 2, 12"
-                />
-                <ExpenseItem
-                    title="Car Insurance"
-                    amount="294.67"
-                    date="2021, 2, 28"
-                />
-                <ExpenseItem
-                    title="New Wood Desk"
-                    amount="450.00"
-                    date="2021, 5, 12"
-                />
-                <ExpenseItem
-                    title="Microcenter"
-                    amount="135.45"
-                    date="2021, 6, 1"
-                />
-                <ExpenseItem
-                    title="Computer"
-                    amount="985.99"
-                    date="2021, 6, 25"
-                />
+                <ExpenseList />
             </AppContainer>
         </AppWrapper>
     );
---
+++ src/components/ExpenseList/ExpenseListStyle.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+    padding: 1rem;
+    border: 1px solid #1a2634;
+    border-radius: 3rem;
+    background: #39a6a3;
+`;
