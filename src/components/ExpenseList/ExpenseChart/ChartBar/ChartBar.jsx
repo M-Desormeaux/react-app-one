4370cff22fa01d5c0c7a1104015c409f08633200
Michael <michael.desormeaux@scswla.org>
2021-07-12 10:03:42 -0500

    feat: ✨ add functional bars, needs reducer next

--- src/components/ExpenseList/ExpenseChart/ChartBar/ChartBar.jsx
+++ src/components/ExpenseList/ExpenseChart/ChartBar/ChartBar.jsx
@@ -9,10 +9,42 @@ const Container = styled.div`
     }
 `;
 
+const DynamicBar = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const BarBody = styled.div`
+    height: 100px;
+    width: 20px;
+    background: red;
+
+    display: flex;
+    align-items: flex-end;
+`;
+const BarFill = styled.div`
+    width: 100%;
+    height: ${(props) => props.heightValue}%;
+    background: green;
+`;
+const BarLabel = styled.div``;
+
 export const ChartBar = (props) => {
     return (
         <Container>
-            <>{props.value / props.maxValue}</>
+            <DynamicBar>
+                <BarBody>
+                    <BarFill
+                        heightValue={(props.value / props.maxValue) * 100}
+                    />
+                </BarBody>
+                <BarLabel>
+                    <>{props.month}</>
+                    <br />
+                    <>{props.year}</>
+                </BarLabel>
+            </DynamicBar>
         </Container>
     );
 };
