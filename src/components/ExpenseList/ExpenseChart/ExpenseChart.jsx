a940386803a2adb4b145ca5279a8eed4d6339b3a
Michael <michael.desormeaux@scswla.org>
2021-07-13 08:07:41 -0500

    feat: ✨ fixed compounding issue with listed expenses

--- src/components/ExpenseList/ExpenseChart/ExpenseChart.jsx
+++ src/components/ExpenseList/ExpenseChart/ExpenseChart.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ChartBar } from "./ChartBar/ChartBar";
-import { chartMonths, chartYears, thisMonth } from "./ExpenseCharts";
 import {
     ExpenseChartContainer,
     ExpenseChartGraph,
@@ -22,6 +21,106 @@ export const ExpenseChart = (props) => {
             : selectedYear === "all"
             ? selectedMonth + " History"
             : selectedMonth + " " + selectedYear + " History";
+
+    const chartMonths = [
+        {
+            key: 01,
+            label: "Jan",
+            value: 0,
+        },
+        {
+            key: 02,
+            label: "Feb",
+            value: 0,
+        },
+        {
+            key: 03,
+            key: 0,
+            label: "Mar",
+            value: 0,
+        },
+        {
+            key: 04,
+            label: "Apr",
+            value: 0,
+        },
+        {
+            key: 05,
+            label: "May",
+            value: 0,
+        },
+        {
+            key: 06,
+            label: "Jun",
+            value: 0,
+        },
+        {
+            key: 07,
+            label: "Jul",
+            value: 0,
+        },
+        {
+            key: 08,
+            label: "Aug",
+            value: 0,
+        },
+        {
+            key: 09,
+            label: "Sep",
+            value: 0,
+        },
+        {
+            key: 10,
+            label: "Oct",
+            value: 0,
+        },
+        {
+            key: 11,
+            label: "Nov",
+            value: 0,
+        },
+        {
+            key: 12,
+            label: "Dec",
+            value: 0,
+        },
+    ];
+
+    const chartYears = [
+        {
+            key: 01,
+            label: "2020",
+            value: 0,
+        },
+        {
+            key: 02,
+            label: "2021",
+            value: 0,
+        },
+        {
+            key: 03,
+            label: "2022",
+            value: 0,
+        },
+        {
+            key: 04,
+            label: "2023",
+            value: 0,
+        },
+        {
+            key: 05,
+            label: "2024",
+            value: 0,
+        },
+    ];
+
+    const thisMonth = [
+        {
+            key: 01,
+            label: "",
+            value: 0,
+        },
+    ];
 
     for (const item in props.items) {
         const expenseMonth = props.items[item].date.getMonth();
